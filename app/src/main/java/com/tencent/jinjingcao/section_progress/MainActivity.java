f31b6f23f61efeca4ea53ab1a962147cb3708a43
sparks345 <sparks345@gmail.com>
2017-10-10 20:16:07 +0800

    basic function.

--- app/src/main/java/com/tencent/jinjingcao/section_progress/MainActivity.java
+++ app/src/main/java/com/tencent/jinjingcao/section_progress/MainActivity.java
@@ -1,13 +1,61 @@
 package com.tencent.jinjingcao.section_progress;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements OnClickListener {
+
+    private SectionProgressBar secBar;
+    private Button btnIncreaseProgress;
+    private Button btnAddBlock;
+    private Button btnDelBlock;
+
+    private void assignViews() {
+        secBar = (SectionProgressBar) findViewById(R.id.sec_bar);
+
+        btnIncreaseProgress = (Button) findViewById(R.id.btnIncreaseProgress);
+        btnAddBlock = (Button) findViewById(R.id.btnAddBlock);
+        btnDelBlock = (Button) findViewById(R.id.btnDelBlock);
+
+        btnIncreaseProgress.setOnClickListener(this);
+        btnAddBlock.setOnClickListener(this);
+        btnDelBlock.setOnClickListener(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        assignViews();
+    }
+
+    private void init() {
+        secBar = (SectionProgressBar) findViewById(R.id.sec_bar);
+        secBar.setProgress(25.0f);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnIncreaseProgress:
+                float aimProgress = secBar.getCurrentProgress() + 15.0f;
+                if (aimProgress > 100) {
+                    aimProgress = 100;
+                }
+                secBar.setProgress(aimProgress);
+                break;
+            case R.id.btnAddBlock:
+                secBar.setSplitAtCurrent();
+                break;
+            case R.id.btnDelBlock:
+                if (secBar.selectLastSection()) {
+                    secBar.backDelSection();
+                }
+                break;
+        }
     }
 }
--- app/src/main/java/com/tencent/jinjingcao/section_progress/SectionProgressBar.java
+++ app/src/main/java/com/tencent/jinjingcao/section_progress/SectionProgressBar.java
@@ -1,9 +1,12 @@
 package com.tencent.jinjingcao.section_progress;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
@@ -22,11 +25,17 @@ public class SectionProgressBar extends View {
     private final Paint mSplitBlockPaint;
     private final RectF mRect;
 
-    // all sections in current progress bar
-    private ArrayList<Section> sections = new ArrayList<>();
+    // all sections in current progress bar.
+    private ArrayList<Section> mSections = new ArrayList<>();
 
     // current progress.
     private float mCurrentProgress;
+
+    // block width.
+    private int mBlockWidth = 6 * 2;
+
+    private int mAlpha = 100;
+    private AnimationTimer mAnimTimer;
 
     public SectionProgressBar(Context context) {
         this(context, null);
@@ -39,15 +48,20 @@ public class SectionProgressBar extends View {
         this.mRect = new RectF();
 //        this.mPaint.setAntiAlias(true);
         this.mSplitBlockPaint = new Paint();
+        this.mSplitBlockPaint.setColor(getResources().getColor(R.color.colorPrimary));
+
+        this.mPaint.setColor(getResources().getColor(R.color.colorAccent));
+        this.mPaint.setStrokeWidth(20);
     }
 
-    // attributes
+    // attributes:
     // split_block_color
     // split_block_width
-    // progress_color
-    // progress_height
-    // section_anim_on_select
-    // section_on_focusable
+    // split_block_height
+    // progress_bar_color
+    // progress_bar_height
+    // section_anim_on_selection
+    // section_selectable_by_click
 
     /**
      * set progress for view
@@ -65,16 +79,56 @@ public class SectionProgressBar extends View {
     /**
      * set split block at giving progress, and add a new section automatic.
      *
-     * @param percent percent of split block
+     * @param percentEnd percent of split block
      */
-    public void setSplitAtProgress(float percent) {
+    private void setSplitAtProgress(float percentEnd) {
         Section lastSection = null;
-        Section newSection = new Section();
-        if (sections.size() > 0) {
-            lastSection = sections.get(sections.size() - 1);
+
+        if (mSections.size() > 0) {
+            lastSection = mSections.get(mSections.size() - 1);
         }
-        newSection.start = lastSection != null ? lastSection.end : 0;
-        newSection.end = percent;
+
+        float percentStart = lastSection != null ? lastSection.end : 0;
+
+        if (percentStart == percentEnd) {
+            return;
+        }
+
+        if (lastSection != null) {
+            lastSection.setSelection(false);
+        }
+
+        Section newSection = new Section(percentStart, percentEnd);
+        mSections.add(newSection);
+
+        postInvalidate();
+    }
+
+    /**
+     * set split block at current progress.
+     */
+    public void setSplitAtCurrent() {
+        setSplitAtProgress(mCurrentProgress);
+    }
+
+    /**
+     * delete last section.
+     */
+    public void backDelSection() {
+        if (mSections.size() > 0) {
+            Section lastSection = mSections.get(mSections.size() - 1);
+            if (mCurrentProgress > lastSection.end) {
+                throw new RuntimeException("current progress > last block end, add a block first, then do backDelSection.");
+            }
+            mSections.remove(lastSection);
+            setProgress(lastSection.start);
+
+            if (mAnimTimer != null) {
+                mAnimTimer.stop();
+                mAnimTimer = null;
+            }
+        }
+
         postInvalidate();
     }
 
@@ -84,20 +138,233 @@ public class SectionProgressBar extends View {
         int aimPos = (int) (mCurrentProgress * width / 100);
         int centerY = getHeight() / 2;
         mRect.left = getPaddingLeft();
-        mRect.right = getPaddingRight();
+        mRect.right = aimPos + mRect.left;// width - getPaddingRight();
         mRect.top = centerY - 10;
         mRect.bottom = centerY + 10;
         canvas.drawRect(mRect, mPaint);
+
+        for (Section section : mSections) {
+            section.doDraw(canvas);
+        }
+
         super.onDraw(canvas);
+    }
+
+    public float getCurrentProgress() {
+        return mCurrentProgress;
+    }
+
+    public boolean selectLastSection() {
+        if (mSections.size() > 0) {
+            Section lastSection = mSections.get(mSections.size() - 1);
+            if (mCurrentProgress > lastSection.end) {
+                throw new RuntimeException("current progress > last block end, add a block first, then do selectLastSection.");
+            }
+            boolean ret = lastSection.mIsSelected;
+            lastSection.setSelection(true);
+            postInvalidate();
+            return ret;
+        }
+
+        return false;
     }
 
     private class Section {
 
-        private boolean mIsSelected;
+        private final Paint mBlockPaint;
 
-        private Paint mPaint = new Paint();
+        private final Paint mPaint;
+        private RectF mSectionRect;
+        private RectF mBlockRect;
+        private AnimationOnSelection mAnimRunable;
+
+        public Section(float start, float end) {
+            this.mPaint = new Paint();
+            this.mPaint.setColor(getResources().getColor(R.color.colorAccent));
+            this.mBlockPaint = mSplitBlockPaint;
+            this.start = start;
+            this.end = end;
+        }
+
+        private boolean mIsSelected;
 
         float start;
         float end;
+
+        public void doDraw(Canvas canvas) {
+            int centerY = getHeight() / 2;
+
+            if (mSectionRect == null) {
+                mSectionRect = new RectF();
+                int offsetLeft = getPaddingLeft();
+                int width = getWidth() - offsetLeft - getPaddingRight();
+                int startPos = (int) (start * width / 100) + offsetLeft;// - mBlockWidth / 2;
+                int endPos = (int) (end * width / 100) + offsetLeft;// - mBlockWidth / 2;
+
+                mSectionRect.left = startPos;
+                mSectionRect.right = endPos;
+
+                mSectionRect.top = centerY - 10;
+                mSectionRect.bottom = centerY + 10;
+            }
+
+            if (mBlockRect == null) {
+                mBlockRect = new RectF();
+                int blockStartPos = (int) mSectionRect.right - mBlockWidth / 2;
+                int blockEndPos = blockStartPos + mBlockWidth / 2;
+
+                mBlockRect.left = blockStartPos;
+                mBlockRect.right = blockEndPos;
+                mBlockRect.top = centerY - 15;
+                mBlockRect.bottom = centerY + 15;
+            }
+
+            if (mIsSelected && mAnimTimer != null) {
+                int alpha = mAnimTimer.getAlpha();
+                mPaint.setAlpha(alpha);
+                mPaint.setStrokeWidth(alpha / 2);
+                mPaint.setColor(Color.parseColor("#ff00ff"));
+            }
+            canvas.drawRect(mSectionRect, mPaint);
+            canvas.drawRect(mBlockRect, mBlockPaint);
+
+        }
+
+        public void setSelection(boolean flag) {
+            mIsSelected = flag;
+
+            if (flag && mAnimTimer == null) {
+                mAnimTimer = new AnimationTimer();
+                mAnimTimer.start();
+            } else if (!flag && mAnimTimer != null) {
+                mAnimTimer.stop();
+            }
+        }
+    }
+
+    private class AnimationTimer {
+        TimerTask timerTask;
+        Timer timer;
+        int alpha = 100;
+
+        byte animDirection = 1;
+        private boolean mStop;
+
+        public AnimationTimer() {
+            timer = new Timer();
+            timerTask = new TimerTask() {
+                @Override
+                public void run() {
+//                    while (!mStop) {
+                    switch (animDirection) {
+                        case 0:
+                            if (alpha < 100) {
+                                alpha += 5;
+                                postInvalidate();
+                            } else {
+                                alpha = 100;
+                                animDirection = 1;
+                            }
+                            break;
+                        case 1:
+                            if (alpha > 0) {
+                                alpha -= 5;
+                                postInvalidate();
+                            } else {
+                                alpha = 0;
+                                animDirection = 0;
+                            }
+                            break;
+                    }
+//                    }
+
+                    // reset
+//                    mAlpha = 100;
+//                    animDirection = 1;
+                }
+            };
+        }
+
+        public void start() {
+            timer.schedule(timerTask, 500, 500);
+        }
+
+        private void stop() {
+            if (this.timerTask != null) {
+                this.timerTask.cancel();
+                this.timerTask = null;
+            }
+
+            if (this.timer != null) {
+                this.timer.cancel();
+                this.timer = null;
+            }
+        }
+
+        public int getAlpha() {
+            return alpha;
+        }
+    }
+
+    /**
+     * make selection anim work.
+     */
+    private class AnimationOnSelection implements Runnable {
+        private final Thread thread;
+        byte animDirection = 1;
+        private boolean mStop;
+
+        public AnimationOnSelection() {
+            this.thread = new Thread(this);
+            this.thread.start();
+        }
+
+        public void stop() {
+            mStop = true;
+        }
+
+        @Override
+        public void run() {
+            while (!mStop) {
+                switch (animDirection) {
+                    case 0:
+                        if (mAlpha < 100) {
+                            try {
+                                synchronized (thread) {
+                                    mAlpha += 5;
+                                    thread.wait(1500);
+                                }
+                                postInvalidate();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            mAlpha = 100;
+                            animDirection = 1;
+                        }
+                        break;
+                    case 1:
+                        if (mAlpha > 0) {
+                            try {
+                                synchronized (thread) {
+                                    mAlpha -= 5;
+                                    thread.wait(1500);
+                                }
+                                postInvalidate();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            mAlpha = 0;
+                            animDirection = 0;
+                        }
+                        break;
+                }
+            }
+
+            // reset
+            mAlpha = 100;
+            animDirection = 1;
+        }
     }
 }
